2cd6685f7c7cbb5d4ee0d67b42e55fd7a30e1797
Nitin Pal <141559894+nitinpal90@users.noreply.github.com>
2024-06-24 12:55:59 +0530

    Add files via upload

---
+++ Day-1 of Python Challenge.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day -1 of Python Challenge 
+
+# We are going to write our first programe
+
+# In[2]:
+
+
+print("Hello World!")
+
+
+# In[3]:
+
+
+print(5+5)
+
+
+# # How to write the comment in programe
+
+# In[4]:
+
+
+#This is Comment
+
+print("Python")
+
+
+# # Mulitiline Comments
+
+# In[9]:
+
+
+""" This is our first class in python """
+
+print(6+5)
+
